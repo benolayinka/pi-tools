445539ca24955e5059ffd8ab52eaa63a7ab77da5
Ben Olayinka <ben.olayinka@gmail.com>
2020-02-04 13:35:16 +0100

    Update install_robot.sh

--- install_robot.sh
+++ install_robot.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-cp ./config.txt /boot/config.txt
+read -p 'did you create .env?: ' confirm
+
+if ($confirm "!=" y && $confirm != "Y"); then
+  exit 1
+fi
+
+sudo cp config.txt /boot/config.txt
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
@@ -12,5 +18,11 @@ git clone https://github.com/benolayinka/rover-client.git
 cd rover-client
 
 sudo apt install nodejs npm -y
-xargs sudo apt -y install < dependencies
 npm install
+xargs sudo apt -y install < dependencies
+
+sudo npm install pm2 -g
+pm2 start gstreamer.sh
+pm2 start johnny.js
+pm2 save
+pm2 startup
